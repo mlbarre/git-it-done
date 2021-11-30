c98724422a0c258df8f3c650d4f268689c6b35c3
mlb <mlbarrera1538@gmail.com>
2021-11-30 01:47:08 -0600

    restructure js file to fetch url repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,14 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+    // FORMAT THE GITHUB API URL
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
+    // MAKE A REQUEST TO THE URL 
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
 };
+ 
 
-getUserRepos();
+getUserRepos("microsoft");

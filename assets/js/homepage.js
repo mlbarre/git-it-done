312e9c49a61523454d9cdc86788f1f2dee9b1f3b
mlbarre <mlbarrera1538@gmail.com>
2021-12-09 16:53:02 -0600

    added final touches to javascript/added new topic buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,7 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
-
+var languageButtonsEl = document.querySelector("#language-buttons")
 
 var getUserRepos = function(user) {
     // FORMAT THE GITHUB API URL
@@ -87,4 +87,33 @@ var displayRepos = function(repos, searchTerm) {
     }
 }
 
+var getFeaturedRepos = function(language) {
+    var apiURL = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiURL).then(function(response) {
+        if (response.ok) {
+            // console.log(response);
+            response.json().then(function(data) {
+                // console.log(data);
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert('Error: GitHub User Not Found');
+        }
+    })
+};
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    // console.log(language);
+
+    if (language) {
+        getFeaturedRepos(language);
+
+        // CLEAR OLD CONTENT
+        repoContainerEl.textContent = "";
+    }
+}
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
